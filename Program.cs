171b85a120e3060fb774924f44b8fd7e8ee1d325
Stefano Savarino <stefano@reachconsulting.se>
2022-10-11 21:00:17 +0200

    Save to file implemented

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to DnDHelper 0.1");
+Console.WriteLine("Insert character name: ");
+string? name = null;
+name = Console.ReadLine();
+Console.WriteLine($"Character: {name}");
+
+FileInfo char_file = new FileInfo("C:/Users/StefanoSavarino/Desktop/characters.txt");
+StreamWriter sw = char_file.CreateText();
+sw.WriteLine("character: " + name);
+sw.Close();
